5a9aca3fdb1afd73e95aecaa885e434bdf47cced
PalSvendsen <pal@getacademy.no>
2024-01-09 13:35:20 +0100

    Add project files.

---
+++ pokemon/Program.cs
@@ -0,0 +1,69 @@
+﻿using pokemon;
+
+namespace PokemonNew
+{
+    class Program
+    {
+        static void Main()
+        {
+            pokemonNNN newPokemon = new pokemonNNN();
+            var pokeList = new List<string>();
+
+            Console.WriteLine("Velkommen til Pokemon app. Hva er navnet på Pokemon treneren?");
+            string trener = Console.ReadLine();
+            Console.WriteLine("Hva er din start pokemon?");
+            Console.WriteLine("1. Pikachu");
+            Console.WriteLine("2. Charmander");
+            Console.WriteLine("3. Vulp");
+            string pokemon = Console.ReadLine().ToLower();
+            if (pokemon != "pikachu" && pokemon != "charmander" && pokemon != "vulp")
+            {
+                Console.WriteLine("Try again");
+                Main();
+            }
+            else
+            {
+                Console.WriteLine($"Velkommen {trener}, du har valgt {pokemon} som din start pokemon!");
+            }
+            //Console.WriteLine($"Velkommen {trener}, du har valgt {pokemon} som din start pokmenon!");
+            pokeList.Add(pokemon);
+            while (true)
+            {
+            Console.WriteLine("Hvor vil du lete etter pokemon?");
+            Console.WriteLine("1. Gress");
+            Console.WriteLine("2. Vann");
+            Console.WriteLine("3. Hule");
+            Console.WriteLine("4. Bakke");
+            Console.WriteLine("5. Inventory");
+            Console.WriteLine("6. Pokeshop");
+
+            string valg = Console.ReadLine();
+
+            switch (valg)
+            {
+                case "1":
+                    newPokemon.Gress();
+                    break;
+                case "2":
+                    newPokemon.Vann(); 
+                    break;
+                case "3":
+                    newPokemon.Hule();
+                    break;
+                case "4":
+                    newPokemon.Bakke();
+                    break;
+                case "5":
+                    newPokemon.CoachInventory();
+                    break;  
+                case "6":
+                    newPokemon.PokeShop();
+                    break;
+                default:
+                    Console.WriteLine("Ugyldig. Prøv igjen!");
+                    break;
+            }
+        }
+    }
+}
+}
---
+++ pokemon/pokemon.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace pokemon
+{
+    public class pokemonNNN
+    {
+        public List<string> pokeList = new List<string>();
+        private int Pokeballs;
+        private int Potion;
+        private int Credits = 100;
+        public void Gress()
+        {
+            Pokemon[] GressPokemon = { new Pokemon { Name = "Bulbasaur", Health = RandomizeStat(), Strength = RandomizeStat() },
+                new Pokemon { Name = "Oddish", Health = RandomizeStat(), Strength =  RandomizeStat() },
+                new Pokemon { Name = "Bellsprout", Health = RandomizeStat(), Strength =  RandomizeStat() },
+                new Pokemon { Name = "Tangela", Health = RandomizeStat(), Strength =  RandomizeStat() } };
+            Random pokeRandom  = new Random();
+            int pokeIndex = pokeRandom.Next(GressPokemon.Length);
+            Pokemon pokeString = GressPokemon[pokeIndex];
+            Console.WriteLine($"Du fant {pokeString.Name}. Han har {pokeString.Health}hp og {pokeString.Strength} styrke!");
+            Console.WriteLine();
+            Console.WriteLine("Hva vil du gjøre?");
+            Console.WriteLine("1. Gå i kamp?");
+            Console.WriteLine("2. Prøve å fange");
+
+            string valg = Console.ReadLine();
+
+            switch (valg)
+            {
+                case "1":
+                    Console.WriteLine("Du gikk i kamp!");
+                    HandleBattle(GressPokemon[pokeIndex].Name);
+                    break;
+                case "2":
+                    Console.WriteLine("Du prøver å fange Pokémon!");
+                    HandleCatch(GressPokemon[pokeIndex].Name);
+                    break;
+                default:
+                    Console.WriteLine("Ugyldig valg.");
+                    break;
+            }
+        }
+
+        public void Vann()
+        {
+            Pokemon[] VannPokemon =
+            {
+                new Pokemon { Name = "Squirtle", Health = RandomizeStat(), Strength = RandomizeStat() },
+                new Pokemon { Name = "Todotile", Health = RandomizeStat(), Strength = RandomizeStat() },
+                new Pokemon { Name = "Mudkip", Health = RandomizeStat(), Strength = RandomizeStat() },
+                new Pokemon { Name = "Piplup", Health = RandomizeStat(), Strength = RandomizeStat() }
+            };
+            Random pokeRandom = new Random();
+            int pokeIndex = pokeRandom.Next(VannPokemon.Length);
+            Pokemon pokeString = VannPokemon[pokeIndex];
+            Console.WriteLine($"Du fant {pokeString.Name}. Han har {pokeString.Health}hp og {pokeString.Strength} styrke!");
+            Console.WriteLine();
+            Console.WriteLine("Hva vil du gjøre?");
+            Console.WriteLine("1. Gå i kamp?");
+            Console.WriteLine("2. Prøve å fange");
+
+            string valg = Console.ReadLine();
+
+            switch (valg)
+            {
+                case "1":
+                    Console.WriteLine("Du gikk i kamp!");
+                    HandleBattle(VannPokemon[pokeIndex].Name);
+                    break;
+                case "2":
+                    Console.WriteLine("Du prøver å fange Pokémon!");
+                    HandleCatch(VannPokemon[pokeIndex].Name);
+                    break;
+                default:
+                    Console.WriteLine("Ugyldig valg.");
+                    break;
+            }
+        }
+
+        public void Hule()
+        {
+            Pokemon[] HulePokemon =
+            {
+                new Pokemon { Name = "Zubat", Health = RandomizeStat(), Strength = RandomizeStat() },
+                new Pokemon { Name = "Geodude", Health = RandomizeStat(), Strength = RandomizeStat() },
+                new Pokemon { Name = "Onix", Health = RandomizeStat(), Strength = RandomizeStat() },
+                new Pokemon { Name = "Aron", Health = RandomizeStat(), Strength = RandomizeStat() }
+            };
+            Random pokeRandom = new Random();
+            int pokeIndex = pokeRandom.Next(HulePokemon.Length);
+            Pokemon pokeString = HulePokemon[pokeIndex];
+            Console.WriteLine($"Du fant {pokeString.Name}. Han har {pokeString.Health}hp og {pokeString.Strength} styrke!");
+            Console.WriteLine();
+            Console.WriteLine("Hva vil du gjøre?");
+            Console.WriteLine("1. Gå i kamp?");
+            Console.WriteLine("2. Prøve å fange");
+
+            string valg = Console.ReadLine();
+
+            switch (valg)
+            {
+                case "1":
+                    Console.WriteLine("Du gikk i kamp!");
+                    HandleBattle(HulePokemon[pokeIndex].Name);
+                    break;
+                case "2":
+                    Console.WriteLine("Du prøver å fange Pokémon!");
+                    HandleCatch(HulePokemon[pokeIndex].Name);
+                    break;
+                default:
+                    Console.WriteLine("Ugyldig valg.");
+                    break;
+            }
+        }
+
+        public void Bakke()
+        {
+            Pokemon[] BakkePokemon =
+            {
+                new Pokemon { Name = "Sandshrew", Health = RandomizeStat(), Strength = RandomizeStat() },
+                new Pokemon { Name = "Diglett", Health = RandomizeStat(), Strength = RandomizeStat() },
+                new Pokemon { Name = "Phanpy", Health = RandomizeStat(), Strength = RandomizeStat() },
+                new Pokemon { Name = "Trapinch", Health = RandomizeStat(), Strength = RandomizeStat() }
+            };
+            Random pokeRandom = new Random();
+            int pokeIndex = pokeRandom.Next(BakkePokemon.Length);
+            Pokemon pokeString = BakkePokemon[pokeIndex];
+            Console.WriteLine($"Du fant {pokeString.Name}. Han har {pokeString.Health}hp og {pokeString.Strength} styrke!");
+            Console.WriteLine();
+            Console.WriteLine("Hva vil du gjøre?");
+            Console.WriteLine("1. Gå i kamp?");
+            Console.WriteLine("2. Prøve å fange");
+
+            string valg = Console.ReadLine();
+
+            switch (valg)
+            {
+                case "1":
+                    Console.WriteLine("Du gikk i kamp!");
+                    HandleBattle(BakkePokemon[pokeIndex].Name);
+                    break;
+                case "2":
+                    HandleCatch(BakkePokemon[pokeIndex].Name);
+                    break;
+                default:
+                    Console.WriteLine("Ugyldig valg.");
+                    break;
+            }
+        }
+
+        private void HandleBattle(string pokemonName)
+        {
+            string[] fightOrFlight = { $"{pokemonName} rømte", $"{pokemonName} slår tilbake!" };
+            Random FightOrFlight = new Random();
+            int fightIndex = FightOrFlight.Next(fightOrFlight.Length);
+
+
+            Console.WriteLine(fightOrFlight[fightIndex]);
+        }
+
+        private void HandleCatch(string pokemonName)
+        {
+            string[] caught = { $"Du fanget {pokemonName}!", $"{pokemonName} rømte!" };
+            Random catchRandom = new Random();
+            int catchIndex = catchRandom.Next(caught.Length);
+
+            Console.WriteLine(caught[catchIndex]);
+
+            if (caught[catchIndex] == $"Du fanget {pokemonName}!")
+            {
+                pokeList.Add(pokemonName);
+            }
+        }
+
+        public void CoachInventory()
+        {
+            Console.WriteLine($"Du har {Potion} potion(s) and {Pokeballs} pokeball(s) and {Credits} credit(s) left!");
+            foreach (var poke in pokeList)
+            {
+                Console.WriteLine($"-{poke}");
+            }
+        }
+       public int RandomizeStat()
+        {
+            Random random = new Random();
+            return random.Next(1, 200); 
+        }
+
+       public void PokeShop()
+       {
+           Console.WriteLine("Velkommen til PokeShop. Hva vil du ha?");
+           Console.WriteLine("1. Pokeball cost: 30CR");
+           Console.WriteLine("2. Potion cost: 40CR");
+
+           string valg = Console.ReadLine();
+
+           switch (valg)
+           {
+                case "1":
+                    Pokeballs++;
+                    Credits -= 30;
+                    Console.WriteLine($"Du har kjøpt en pokeball. Du har nå {Pokeballs} pokeballs tilgjengelig");
+                    break;
+                case "2":
+                    Potion++;
+                    Credits -= 40;
+                    Console.WriteLine($"Du har kjøp en potion. Du har nå {Potion} tilgjengelig");
+                    break;
+           }
+       }
+    }
+    class Pokemon
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Strength { get; set; }
+    }
+}
